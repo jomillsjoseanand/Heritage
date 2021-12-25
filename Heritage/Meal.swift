b9138c6ff67adba5c7fb8f3e282636b0f70ba3df
jomillsjoseanand <44730936+jomillsjoseanand@users.noreply.github.com>
2021-12-24 21:03:03 -0500

    Added file Meal.swift that defines the type of information received via JSON files from the database.

---
+++ Heritage/Meal.swift
@@ -0,0 +1,19 @@
+//
+//  Meal.swift
+//  Heritage
+//
+//  Created by Jomills Jose Anand on 2021-12-24.
+//
+
+import Foundation
+
+struct Meal {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let place: String
+    let latitude: Double
+    let longitude: Double
+}
+ 
